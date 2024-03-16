bcccda7d813aee6d789846daf1cf11f3d5d30a99
Andressa Silva <andressa.2003abc@gmail.com>
2024-03-16 00:00:55 -0300

    Fist Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConquistasComponent } from './conquistas/conquistas.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    ConquistasComponent,
+    ContatoComponent,
+    HabilidadesComponent,
+    InicioComponent,
+    MenuComponent,
+    ProjetosComponent,
+    SobreComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ConquistasComponent } from './conquistas/conquistas.component';
+import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "inicio",
+    component: InicioComponent
+  },
+  {
+    path: "menu",
+    component: MenuComponent
+  },
+  {
+    path: "sobre",
+    component: SobreComponent
+  },
+  {
+    path: "habilidades",
+    component: HabilidadesComponent
+  },
+  {
+    path: "projetos",
+    component: ProjetosComponent
+  },
+  {
+    path: "conquistas",
+    component: ConquistasComponent
+  },
+  {
+    path: "contato",
+    component: ContatoComponent
+  }
+
+
+];
