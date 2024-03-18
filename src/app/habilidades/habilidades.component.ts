88278a1291697b973badc3b55e90893bd39848dc
Andressa Silva <andressa.2003abc@gmail.com>
2024-03-17 21:50:32 -0300

    Habilidades Component

--- src/app/habilidades/habilidades.component.ts
+++ src/app/habilidades/habilidades.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-habilidades',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './habilidades.component.html',
   styleUrl: './habilidades.component.scss'
 })
