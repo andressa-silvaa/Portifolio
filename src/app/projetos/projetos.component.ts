0e4fcc33fd3ab84a9b5aeb3317cccf1f9091a42a
Andressa Silva <andressa.2003abc@gmail.com>
2024-07-25 19:48:55 -0300

    Feat: Version 1

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+declare var UIkit: any;
 
 @Component({
   selector: 'app-projetos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projetos.component.html',
-  styleUrl: './projetos.component.scss'
+  styleUrls: ['./projetos.component.scss']
 })
 export class ProjetosComponent {
+  public linkRep = "";
+  public linkApp = "";
+  public texto = "";
 
+  openModal(idModal : string, linkRep : string, linkApp : string , idTexto : string) {
+    this.linkApp = linkApp;
+    this.linkRep = linkRep
+    const element = document.getElementById(idTexto);
+    if (element) {
+      this.texto = element.innerText;
+    }
+    UIkit.modal(idModal).show();
+  }
 }
